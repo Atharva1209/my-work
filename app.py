b7db2a930d60bbee710fca84829d285e1c52057e
Atharva1209 <98884031+Atharva1209@users.noreply.github.com>
2022-06-30 18:57:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -249,12 +249,12 @@
 #print(emoji_converter(message))
 
 #Exception case
-try:
-    age= int(input("Age: "))
-    income = 20000
-    risk = income / age
-    print(age, risk)
-except ZeroDivisionError:
-    print("Age cannot be 0!")
-except ValueError:
-    print("Invalid Input")
+#try:
+#    age= int(input("Age: "))
+#    income = 20000
+#    risk = income / age
+#    print(age, risk)
+#except ZeroDivisionError:
+#    print("Age cannot be 0!")
+#except ValueError:
+#    print("Invalid Input")
