657168453914cc334df56b612b1749adc937e1e0
Atharva1209 <98884031+Atharva1209@users.noreply.github.com>
2022-07-01 14:30:39 +0530

    my latest code

--- app.py
+++ app.py
@@ -259,4 +259,34 @@
 #except ValueError:
 #    print("Invalid Input")
 
-#fyfhky
+#CLASS
+
+#class Point:
+#    def __init__(self, x, y):
+#        self.x = x
+#        self.y = y
+#
+#
+#    def move(self):
+#       print("move")
+#
+#    def draw(self):
+#        print("draw")
+#
+#
+#point1= Point(10, 20)
+#point1.x = 11
+#print(point1.x)
+
+#class Person:
+#    def __init__(self, name): #<---- This is a Constructor
+#        self.name = name
+#
+#    def talk(self):
+#        print(f"Hi, I'm {self.name}")
+#
+#john = Person("John Smith")
+#john.talk()
+#
+#bob = Person("Bob Smith")
+#bob.talk()
